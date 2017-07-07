28f32f39af5992c95f1635b4cf9a892f8b142151
Willian Balmant <willchb@gmail.com>
2017-07-07 02:51:12 -0700

    Tests

---
+++ test/hello.js
@@ -0,0 +1,57 @@
+/* eslint-disable */
+const assert = require("assert");
+const path = require("path");
+const translateLoader = require("../index");
+const helloNLS = require("./nls/hello_nls.json");
+
+const allHelloNLS = {
+    "root": helloNLS,
+    "en": require("./nls/en/hello_nls.json"),
+    "en-US": require("./nls/en-US/hello_nls.json"),
+    "fr": require("./nls/fr/hello_nls.json"),
+};
+
+describe("hello", function() {
+    const mod = { };
+    const context = {
+        query: "?locales=en,en-US,fr",
+        resourcePath: path.resolve(__dirname, "nls", "hello_nls.json"),
+    };
+    const source = JSON.stringify(helloNLS);
+    const body = translateLoader.call(context, source);
+    const def = new Function("module", body);
+
+    beforeEach(function() {
+        delete mod.exports;
+    });
+
+    it("should return default", function() {
+        //global.locale = "pt";
+        def.call({ }, mod);
+        assert.equal(JSON.stringify(mod.exports), JSON.stringify(allHelloNLS.root));
+    });
+
+    it("should return english", function() {
+        locale = "en";
+        def.call({ }, mod);
+        assert.equal(JSON.stringify(mod.exports), JSON.stringify(allHelloNLS.en));
+    });
+
+    it("should still return english", function() {
+        global.locale = "en-IN";
+        def.call({ }, mod);
+        assert.equal(JSON.stringify(mod.exports), JSON.stringify(allHelloNLS.en));
+    });
+
+    it("should return american english", function() {
+        global.locale = "en-US";
+        def.call({ }, mod);
+        assert.equal(JSON.stringify(mod.exports), JSON.stringify(allHelloNLS["en-US"]));
+    });
+
+    it("should return french", function() {
+        global.locale = "fr";
+        def.call({ }, mod);
+        assert.equal(JSON.stringify(mod.exports), JSON.stringify(allHelloNLS.fr));
+    });
+});
